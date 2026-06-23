e2b2c6b4c377277f9ef206c22eb1bb4df9745e5a
Janik Teege <account+git@janikteege.me>
2026-06-23 09:48:21 +0200

    runc

--- examples/train_loop.sh
+++ examples/train_loop.sh
@@ -1,17 +1,12 @@
-for gamma in 0.95 0.99; do
-  for ppo_epoch in 3 5 7; do
-    for recurrent in False True; do
+for agg in mean prod; do
+  for recn in 1 2; do
       python train.py \
         --env hemac \
         --algo mappo \
         --load_config ./../tuned_configs/hemac/ippo/config.json \
-        --exp_name "runB_g${gamma}_ppo${ppo_epoch}_rec${recurrent}" \
-        --gamma $gamma \
-        --ppo_epoch $ppo_epoch \
-        --use_recurrent_policy $recurrent \
-        --use_naive_recurrent_policy False \
-        --action_aggregation mean \
-        --share_param False
+        --exp_name "runC_agg${agg}_recn${recn}" \
+        --action_aggregation $agg \
+	--recurrent_n $recn
     done
   done
 done
