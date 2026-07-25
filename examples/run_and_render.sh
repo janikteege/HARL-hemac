8754cef725ba3e154303f12a7198308853f807c7
Janik Teege <account+git@janikteege.me>
2026-07-25 13:43:03 +0200

    fix render script

--- examples/run_and_render.sh
+++ examples/run_and_render.sh
@@ -2,5 +2,7 @@ python train.py \
   --algo mappo \
   --env hemac \
   --exp_name test_render \
-  render.use_render True \
-  train.model_dir "./results/hemac/hemac/mappo/test/seed-00001-2026-06-04-16-18-24/models"
+  --use_render True \
+  --load_config "./../tuned_configs/hemac/ippo/config.json" \
+  --model_dir "./results/hemac/hemac/mappo/new_state_recn1/seed-06636-2026-07-25-13-17-47/models" \
+  --render_ratio 1.2
