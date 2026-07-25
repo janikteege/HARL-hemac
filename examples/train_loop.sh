e93f24f0d3a458383d1060dae216c5446c18a621
Janik Teege <account+git@janikteege.me>
2026-07-25 14:04:42 +0200

    extended_state run

--- examples/train_loop.sh
+++ examples/train_loop.sh
@@ -1,8 +1,25 @@
-for recn in 1 2; do
-	python -Xfrozen_modules=off train.py \
-		--env hemac \
-		--algo mappo \
-		--load_config "./../tuned_configs/hemac/ippo/config.json" \
-		--exp_name "new_state_recn${recn}" \
-		--recurrent_n $recn
+#!/usr/bin/env bash
+set -euo pipefail
+
+BASE_CONFIG="./../tuned_configs/hemac/ippo/config.json"
+
+for hs in 128 256; do
+	for lr in 1e-4 3e-4; do
+		for gamma in 0.95 0.99; do
+			for ent in 0.01 0.05; do
+				  exp="extended_state_hs${hs}_lr${lr}_g${gamma}_ent${ent}"
+
+				  python -Xfrozen_modules=off train.py \
+				    --env hemac \
+				    --algo mappo \
+				    --load_config "${BASE_CONFIG}" \
+				    --exp_name "${exp}" \
+				    --hidden_sizes "[${hs}","${hs}]" \
+				    --lr "${lr}" \
+				    --critic_lr "${lr}" \
+				    --gamma "${gamma}" \
+				    --entropy_coef "${ent}"
+			done
+		done
+	done
 done
