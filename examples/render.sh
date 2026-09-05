1f72c361a96698f802bb1a7b7713e7f3d57a303e
Janik Teege <account+git@janikteege.me>
2026-09-05 13:02:28 +0200

    negative reward for moving away from goal

--- examples/render.sh
+++ examples/render.sh
@@ -1,5 +1,5 @@
 python train.py \
-	--load_config "../results/hemac/hemac/happo/020_reward_going_to_goal/seed-00001-2026-09-03-21-45-32/config.json" \
-	--model_dir "../results/hemac/hemac/happo/020_reward_going_to_goal/seed-00001-2026-09-03-21-45-32/models"\
+	--load_config "../results/hemac/hemac/happo/021_reward_going_to_goal_zero_sum/seed-00001-2026-09-05-12-49-27/config.json" \
+	--model_dir "../results/hemac/hemac/happo/021_reward_going_to_goal_zero_sum/seed-00001-2026-09-05-12-49-27/models"\
 	--torch_threads 1 \
 	--use_render True \
--- examples/train.sh
+++ examples/train.sh
@@ -1,3 +1,3 @@
 python train.py \
-	--exp_name "020_reward_going_to_goal" \
+	--exp_name "021_reward_going_to_goal_zero_sum" \
 	--load_config train_config.json
